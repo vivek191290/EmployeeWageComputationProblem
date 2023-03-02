e97d323c0007fd3b2dc5c1b4cf143a70743a9afc
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-02 17:21:16 +0530

    Add: Check Employee is Present or Absent - Use ((RANDOM)) for Attendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 echo "Welcome to Employee wage computation program"
-
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi

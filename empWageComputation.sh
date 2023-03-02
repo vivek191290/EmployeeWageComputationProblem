48bf22edd907a5ba079fed2060e0e49a3a403edb
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-02 18:15:26 +0530

    Add: Adding Part time Employee And Wage -as per Working hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -15,3 +15,17 @@ salary=$(($empHrs*$empRatePerHr))
 else
 salary=0;
 fi
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3))
+if [ $isFullTime -eq $randomCheck ]
+then
+empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+then
+empHrs=4;
+else
+empHrs=0;
+fi
+salary=$(($empHrs*empRatePerHr))

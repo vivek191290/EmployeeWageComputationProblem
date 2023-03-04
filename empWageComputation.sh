38704c04985dadf8c36b25b2e9e1f7c645d90989
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-04 19:36:54 +0530

    Add: Store the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -35,6 +35,8 @@ randomCheck=$((RANDOM%3));
 	salary=$(($empRatePerHr*$wHour));
 	totalSalary=$(($totalSalary+$salary));
 	((day++));
+echo "Employee Daily Wage: " ${salary[*]}
+echo "Employee Total Wage: " ${totalSalary[@]}
 done
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
 
